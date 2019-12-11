ad68555af353fc4727c7d7fbf639e78760f61584
Jan Brecka <jan.brecka@gmail.com>
2019-12-12 00:26:33 +0100

    day9 + day11

--- src/intCodeComputer.js
+++ src/intCodeComputer.js
@@ -3,6 +3,7 @@ const { int } = require('./lib')
 const modes = {
   MEMORY: 0,
   IMMEDIATE: 1,
+  RELATIVE: 2,
 }
 
 const io = {
@@ -10,6 +11,10 @@ const io = {
   INPUT: 'INPUT',
   OUTPUT: 'OUTPUT',
 }
+
+const ioValues = new Set(Object.values(io))
+const isIO = ({ value }) =>
+  Array.isArray(value) && ioValues.has(value[0])
 
 function parse(program) {
   return program.split(',').map(int)
@@ -27,16 +32,33 @@ function runIntCode(program) {
   function* run(program, read) {
     yield ['IGNORE/INIT']
 
+    let i = 0
+    let base = 0
+
+    const s = (p) => p || 0
     const p1 = (i) => i + 1
     const p2 = (i) => i + 2
     const p3 = (i) => i + 3
 
-    const set = (i, value) => program[program[i]] = value
-    const get = (i, mode) => mode === modes.MEMORY
-      ? program[program[i]]
-      : program[i]
+    const set = (i, value, mode) => {
+      switch (mode) {
+        case modes.RELATIVE: {
+          program[s(program[i]) + base] = value
+          break
+        }
+        default:
+          program[s(program[i])] = value
+      }
+    }
+    const get = (i, mode) => {
+      switch (mode) {
+        case modes.MEMORY: return s(program[s(program[i])])
+        case modes.RELATIVE: return s(program[s(program[i]) + base])
+        default: return s(program[i])
+      }
+    }
 
-    for (let i = 0; i < program.length;) {
+    for (; i < program.length;) {
       const i1 = i + 1
       const i2 = i + 2
       const i3 = i + 3
@@ -60,19 +82,19 @@ function runIntCode(program) {
         case '99':
           return [io.END, get(0, modes.IMMEDIATE)]
         case '01': {
-          set(i3, get1() + get2())
+          set(i3, get1() + get2(), p3Mode)
           i = i + 4
           break
         }
         case '02': {
-          set(i3, get1() * get2())
+          set(i3, get1() * get2(), p3Mode)
           i = i + 4
           break
         }
         case '03': {
           yield [io.INPUT]
           yield ['IGNORE/INPUT']
-          set(i1, read())
+          set(i1, read(), p1Mode)
           i = i + 2
           break
         }
@@ -97,14 +119,19 @@ function runIntCode(program) {
         }
         case '07': {
           const isLess = get1() < get2()
-          set(i3, Number(isLess))
+          set(i3, Number(isLess), p3Mode)
           i = i + 4
           break
         }
         case '08': {
           const isEqual = get1() == get2()
-          set(i3, Number(isEqual))
+          set(i3, Number(isEqual), p3Mode)
           i = i + 4
+          break
+        }
+        case '09': {
+          base = base + get1()
+          i = i + 2
           break
         }
         default:
@@ -125,23 +152,29 @@ function runIntCode(program) {
   ]
 }
 
+
+
 module.exports = {
   modes,
   io,
+  isIO,
   parse,
   runIntCode,
-  runUntilDone: (gen) => {
-    while (!(current = gen.next()).done) {}
-    return current.value
+  runUntilDone: (gen, onOutput = () => {}) => {
+    let current
+    while (true) {
+      current = gen.next()
+      if (isIO(current) && current.value[0] == io.OUTPUT)
+        onOutput(current.value[1])
+      if (current.done) return current.value
+    }
   },
   runUntilIO: (gen) => {
-    const IOValues = new Set(Object.values(io))
-    const isIO = ({ value }) =>
-      Array.isArray(value) && IOValues.has(value[0])
+    let current
     while (true) {
-      const current = gen.next()
+      current = gen.next()
       if (isIO(current)) return current.value
       if (current.done) return
     }
-  }
+  },
 }
