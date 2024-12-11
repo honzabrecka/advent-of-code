9da2e4cc04c6a0e1c7b4dda970e5cf8c73ceb301
Jan Brecka <jan.brecka@gmail.com>
2024-12-11 19:40:34 +0100

    another way

--- src/aoc24/day11.clj
+++ src/aoc24/day11.clj
@@ -44,3 +44,30 @@
 (defn solve-2
   []
   (nth (blink-seq input) 75))
+
+(def count'
+  (memoize
+    (fn
+      ([stone max-level]
+       (count' stone 0 max-level))
+      ([stone level max-level]
+       (let [stone' (str stone)]
+         (cond
+           (= level max-level) 1
+           (= stone 0) (count' 1 (inc level) max-level)
+           (even? (count stone')) (let [[a b] (split-stone stone')]
+                                    (+ (count' a (inc level) max-level)
+                                       (count' b (inc level) max-level)))
+           :else (count' (* stone 2024) (inc level) max-level)))))))
+
+(defn solve-2'
+  []
+  (transduce (map #(count' % 0 75)) + input))
+
+; (time (solve-2))
+; "Elapsed time: 175.81225 msecs"
+; 228449040027793
+
+; (time (solve-2'))
+; "Elapsed time: 145.286167 msecs"
+; 228449040027793
